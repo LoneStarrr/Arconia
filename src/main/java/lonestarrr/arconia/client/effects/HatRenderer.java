8971be2a4d328e8e9d9e4ad1a5022ea8b29d782f
LoneStarrr <lonestarrr83@gmail.com>
2022-01-16 11:31:44 -0500

    Hats now render the resource to be generated

---
+++ src/main/java/lonestarrr/arconia/client/effects/HatRenderer.java
@@ -0,0 +1,37 @@
+package lonestarrr.arconia.client.effects;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+import lonestarrr.arconia.common.block.tile.HatTileEntity;
+import lonestarrr.arconia.common.block.tile.PedestalTileEntity;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
+import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+
+public class HatRenderer extends TileEntityRenderer<HatTileEntity> {
+    public HatRenderer(TileEntityRendererDispatcher dispatcher) {
+        super(dispatcher);
+    }
+
+    @Override
+    public void render(
+            HatTileEntity tileEntity, float partialTicks, MatrixStack matrixStack, IRenderTypeBuffer buffer, int combinedLight,
+            int combinedOverlay) {
+        // TODO Duplicate logic w/ResourceGenRenderer - separate out into shared code
+        ItemStack stack = tileEntity.getResourceGenerated();
+        if (stack == ItemStack.EMPTY) {
+            return;
+        }
+
+        BlockPos tePos = tileEntity.getPos();
+        BlockPos itemPos = tePos.up();
+
+        matrixStack.push();
+        // TER's have the tile entity at (0, 0, 0), compensate
+        matrixStack.translate(-tePos.getX(), -tePos.getY(), -tePos.getZ());
+        ItemProjector.projectItem(stack, itemPos, matrixStack, buffer, combinedLight, combinedOverlay, false);
+
+        matrixStack.pop();
+    }
+}
